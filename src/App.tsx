9dbe4377d63acb00c5a400f047c33cab88558e20
hoantv <hoantv@cyberlotus.com>
2023-02-01 00:09:21 +0700

    init code

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { FC, useState } from 'react'
+import reactlogo from './assets/images/react.svg'
+import './assets/styles/app.css'
+import './assets/styles/app.scss'
+
+const App: FC = () => {
+  const [fullname, setFullname] = useState('Trần Văn Hoàn')
+  return (
+    <div>
+      <img src={reactlogo} alt="React Logo" width={100} height={100} />
+      <h1>{fullname}</h1>
+      <h2>Base react with webpack</h2>
+    </div>
+  )
+}
+
+export default App
